307af6044f64704ce7a94ab8bb15a0a5e0859aba
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-28 23:45:26 -0400

    adapted the home page from react

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,26 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container text-center">
+    <h4 className="fst-italic">Here to stay</h4>
+    <p className="py-3">
+      Target Canada is a leading electronics retailer in the Canadian market,
+      providing customers with a wide range of high-quality electronic products
+      and accessories. Our mission is to provide innovative, cutting-edge
+      technology to our customers, while delivering exceptional service and
+      value. Our selection includes the latest smartphones, laptops, tablets,
+      gaming consoles, home entertainment systems, and more, from top brands in
+      the industry. Our knowledgeable staff are committed to providing customers
+      with expert advice and support, to help them find the perfect product for
+      their needs. Shop with us today and experience the Target Canada
+      difference!
+    </p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
