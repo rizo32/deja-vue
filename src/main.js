e9291832d4588ffdaf19e491a9119c7a8413eac8
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-28 23:44:34 -0400

    import bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import 'bootstrap'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import './assets/css/styles.css'
 
 createApp(App).use(router).mount('#app')
