ac04d8465c3e2c88b8a6df186fdde36b2455f297
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-29 21:03:20 -0400

    adaptation composante coloredLine en vue

---
+++ src/components/ColoredLine.vue
@@ -0,0 +1,31 @@
+<template>
+  <hr :style="lineStyle">
+</template>
+
+<script>
+export default {
+  name: 'ColoredLine',
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: '#000000'
+    }
+  },
+  computed: {
+    lineStyle () {
+      return {
+        color: this.color || '#000000'
+        // backgroundColor: this.color
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+hr {
+  height: 2px;
+  width: 100%;
+}
+</style>
