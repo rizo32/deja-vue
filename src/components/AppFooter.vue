532fe8638232edf8209934aafb9ab018cc46cccc
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-29 21:03:02 -0400

    adaptation footer en vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="container text-center fst-italic pt-5 pb-2">
+      <p>Copyright rizorizo.art {{ year }}</p>
+  </footer>
+</template>
+
+<script>
+
+export default {
+  name: 'AppFooter',
+  props: {
+    year: {
+      type: Number,
+      required: true
+    }
+  }
+}
+</script>
