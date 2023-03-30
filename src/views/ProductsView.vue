d68181b74f1c2fbe98e77e021842ae29f32b32a1
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-29 21:07:16 -0400

    adaptation view list into vue

---
+++ src/views/ProductsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <ul class="container py-1 d-flex flex-column gap-5 w-50">
+    <li class="no-bullets" v-for="product in products" :key="product.id">
+      <SingleProduct :product="product" />
+    </li>
+  </ul>
+</template>
+<script>
+import SingleProduct from '../components/SingleProduct'
+export default {
+  name: 'ProductsView',
+  components: {
+    SingleProduct
+  },
+  props: {
+    products: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+<style>
+
+</style>
