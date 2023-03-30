4649662001d343796d3b9c5895d6b7d0a03f1826
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-29 21:09:54 -0400

    importing font awesome. How awesome

--- src/main.js
+++ src/main.js
@@ -5,5 +5,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/css/styles.css'
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).use(router).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faXmark, faWrench, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faXmark, faWrench, faArrowLeft)
+
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
