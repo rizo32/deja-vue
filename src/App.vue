f5d0920eaf1cd0aa16c11195d0aee8d745592724
Gabriel R. Rizzoli <g.rizzoli@hotmail.ca>
2023-03-29 21:08:07 -0400

    app.vue now fetches the data!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <app-header :routes="routes" title="Target Canada" />
-    <router-view/>
+    <router-view :products="products.products" />
+    <app-footer :year="currentYear" />
   </div>
 </template>
 
@@ -11,18 +12,35 @@
 
 <script>
 import AppHeader from './components/AppHeader'
+import AppFooter from './components/AppFooter'
+import axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    AppHeader
+    AppHeader,
+    AppFooter
+    // products
   },
   data () {
     return {
       routes: [
         // define your routes here
-      ]
+      ],
+      currentYear: new Date().getFullYear(),
+      products: []
     }
+  },
+  created () {
+    axios.get('data/products-db.json')
+      .then(response => {
+        this.products = response.data
+        // console.log(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
   }
 }
 </script>
+<!-- should i put the style here? -->
